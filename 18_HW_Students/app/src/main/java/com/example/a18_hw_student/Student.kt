d29b61260b92fc2b643d8a763697be0341291c27
ElenaVeshtard <h7692306@yandex.ru>
2022-03-29 08:22:34 +0300

    Home work. Students.

---
+++ 18_HW_Students/app/src/main/java/com/example/a18_hw_student/Student.kt
@@ -0,0 +1,35 @@
+package com.example.a18_hw_student
+
+import android.os.Parcel
+import android.os.Parcelable
+
+
+data class Student(val age: Int, val experience: Int, val name: String?) : Parcelable {
+
+
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(age)
+        parcel.writeInt(experience)
+        parcel.writeString(name)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Student> {
+        override fun createFromParcel(parcel: Parcel): Student {
+            return Student(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Student?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
