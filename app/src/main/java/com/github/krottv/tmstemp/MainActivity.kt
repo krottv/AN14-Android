2f80fa7e5acd13d2a7f2dc7eaac4c6106c300d3b
VadimSazonenko <98158170+VadimSazonenko@users.noreply.github.com>
2022-04-19 19:59:13 +0300

    22-Kotlin

--- app/src/main/java/com/github/krottv/tmstemp/MainActivity.kt
+++ app/src/main/java/com/github/krottv/tmstemp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.github.krottv.tmstemp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.krottv.tmstemp.binder.MainActivityDataBinder
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val mainActivityDataBinder: MainActivityDataBinder = MainActivitySceneDataBinder(this)
+        mainActivityDataBinder.bind()
     }
 }
