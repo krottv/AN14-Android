cfe1fff9067a4bd2f16209bed0c744018bf26cea
Vladimir Nazarov <tooldiamond2014@gmail.com>
2022-05-07 01:18:22 +0300

    19 Task AN14

--- app/src/main/java/com/github/krottv/tmstemp/MainActivity.kt
+++ app/src/main/java/com/github/krottv/tmstemp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.github.krottv.tmstemp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import coil.load
+import coil.transform.CircleCropTransformation
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var roundImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        roundImage = findViewById(R.id.img)
+        roundImage.load("https://images.unsplash.com/photo-1505062351414-586330b076f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1080&q=80"){
+            crossfade(true)
+            placeholder(R.drawable.ic_move_icon)
+            transformations(CircleCropTransformation())
+        }
     }
 }
