694070dc97bea7a72461d53e795403766bd07763
ElenaVeshtard <h7692306@yandex.ru>
2022-05-28 15:08:01 +0300

    Change request.

---
+++ app/src/main/java/com/github/krottv/tmstemp/data/remote/DataLoaderFake.kt
@@ -0,0 +1,60 @@
+package com.github.krottv.tmstemp.data.remote
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.delay
+
+private const val CHANNEL_ID = "message"
+
+class DataLoaderFake : DataLoader {
+
+    override suspend fun loadData(
+        notificationManager: NotificationManager,
+        builder: NotificationCompat.Builder
+    ) {
+        loadDataFake(notificationManager, builder)
+    }
+
+    override suspend fun loadNotification(
+        notificationManager: NotificationManager,
+        builder: NotificationCompat.Builder
+    ): Notification {
+        createNotificationChannel(notificationManager)
+        return builder.setContentText("Foreground service").build()
+    }
+
+    private suspend fun loadDataFake(
+        notificationManager: NotificationManager,
+        builder: NotificationCompat.Builder
+    ) {
+        createNotificationChannel(notificationManager)
+        for (i in 1..50) {
+            notificationManager.notify(10, builder.setProgress(50, i, false).build())
+            delay(100)
+        }
+    }
+
+    override suspend fun completeNotification(
+        notificationManager: NotificationManager,
+        builder: NotificationCompat.Builder
+    ) {
+        createNotificationChannel(notificationManager)
+        notificationManager.notify(2, builder.setContentText("is complete").build())
+    }
+
+    override suspend fun createNotificationChannel(notificationManager: NotificationManager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Localized Name"
+            val descriptionText = "Localized Description"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+}
--- app/src/main/java/com/github/krottv/tmstemp/view/AirplaneModeChanged.kt
+++ app/src/main/java/com/github/krottv/tmstemp/view/AirplaneModeChanged.kt
@@ -1,32 +1,34 @@
 package com.github.krottv.tmstemp.view
 
-import android.app.Notification
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import com.github.krottv.tmstemp.R
-import kotlinx.coroutines.delay
+import com.github.krottv.tmstemp.data.remote.DataLoaderFake
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 private const val CHANNEL_ID = "message"
 
 class AirplaneModeChanged : Service() {
+    private val notificationManager =
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
     override fun onCreate() {
         super.onCreate()
 
         runBlocking {
             launch {
-                startForeground(10, loadNotification())
-                loadDataFake()
+                startForeground(
+                    10,
+                    DataLoaderFake().loadNotification(notificationManager, getBuilder())
+                )
+                DataLoaderFake().loadData(notificationManager, getBuilder())
                 stopSelf()
-                completeNotification()
+                DataLoaderFake().completeNotification(notificationManager, getBuilder())
             }
         }
     }
@@ -35,66 +37,14 @@ class AirplaneModeChanged : Service() {
         return START_NOT_STICKY
     }
 
-    private suspend fun loadDataFake() {
-        createNotificationChannel()
-
-        for (i in 1..50) {
-
-            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("Loading")
-
-                .setProgress(50, i, false)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-
-            val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(10, builder.build())
-            delay(100)
-        }
-    }
-
-    private fun loadNotification(): Notification {
-        createNotificationChannel()
-
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle("Loading")
-            .setContentText("Foreground service")
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-
-        return builder.build()
-    }
-
-    private fun completeNotification() {
-        createNotificationChannel()
-
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentTitle("Loading")
-            .setContentText("is complete")
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(2, builder.build())
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Localized Name"
-            val descriptionText = "Localized Description"
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
     override fun onBind(intent: Intent?): IBinder? {
         return null
+    }
+
+    private fun getBuilder(): NotificationCompat.Builder {
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Loading")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
     }
 }
